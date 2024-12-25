a08e8b692e1e183506816d5d344b88369fa44278
icontaylore <icontaylore@icloud.com>
2024-12-25 23:50:46 +0400

    добавил готовый бинарный поиск

--- bsearch/bsearch.go
+++ bsearch/bsearch.go
@@ -1,8 +1,26 @@
 package main
 
+import "fmt"
+
 // Binary search O(n)
 
 func main() {
-	array := []int{5, -4, 3, 2, 1}
+	array := []int{3, 4, 6, 8, 13, 66, 88, 99, 102, 144, 166}
 
+	i := 144
+	l := 0
+	r := len(array) - 1
+
+	for l <= r {
+		mid := (l + r) / 2
+
+		if array[mid] == i {
+			fmt.Println(mid)
+			return
+		} else if array[mid] < i {
+			l = mid
+		} else {
+			r = mid
+		}
+	}
 }
