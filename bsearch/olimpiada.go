d148605d5763fba9fd21107d738bc9870d38c15c
icontaylore <icontaylore@icloud.com>
2024-12-27 14:20:04 +0400

    new binary task

---
+++ bsearch/olimpiada.go
@@ -0,0 +1,27 @@
+package bsearch
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	n := 12 // diploms
+	w := 13 // width
+	h := 12 // high
+
+	inf := math.Max(float64(w), float64(h)) * float64(n)
+	l := 0
+	r := int(inf)
+
+	for l+1 != r {
+		mid := (l + r) / 2
+
+		if (mid/w)*(mid/h) > n {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+	fmt.Println(r)
+}
