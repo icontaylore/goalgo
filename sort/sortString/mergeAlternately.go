3f982b370957bcf4ce681d59a939b0accfe28f69
icontaylore <icontaylore@icloud.com>
2024-12-28 21:01:39 +0400

    newwest

---
+++ sort/sortString/mergeAlternately.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	mergeAlternately("ab", "pqrs")
+
+}
+
+func mergeAlternately(word1 string, word2 string) string {
+	res := ""
+	i, j := 0, 0
+
+	for i < len(word1) || j < len(word2) {
+		if i < len(word1) {
+			res += string(word1[i])
+		}
+
+		if j < len(word2) {
+			res += string(word2[i])
+		}
+		i++
+		j++
+	}
+	return res
+}
