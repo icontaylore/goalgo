d75eed5babb7622a3bc79997d0ce9c177f1b7a3d
icontaylore <icontaylore@icloud.com>
2025-01-07 16:38:11 +0400

    структура + кэш

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 	"time"
 )
 
@@ -23,28 +25,66 @@ type ActionApocalypse struct {
 	t      time.Time
 }
 
+var ActArr = []string{
+	"delete",
+	"reg",
+	"exit",
+	"photo",
+	"foxus",
+	"starty run",
+}
+
 // Метод createTableCahce выводит список действий апокалипсиса и их временные метки
-func (a Apocalypse) createTableCahce() {
-	for _, v := range a.LogChaos {
+func (a Apocalypse) createTableCahce() string {
+	out := ""
+	for i, v := range a.LogChaos {
 		// Используем сохраненное время v.t, а не текущее время time.Now()
-		fmt.Printf("Action: %s, Time: %s\n", v.Action, v.t.Format(time.RFC3339))
+		out = fmt.Sprintf("%d: Action: %s, Time: %s\n", i, v.Action, v.t.Format(time.RFC3339))
 	}
+	return out
 }
 
 func main() {
-	// Создаем объект Tsunami типа Apocalypse
-	Tsunami := Apocalypse{
-		Name:      "Tsunami",   // Название катастрофы
-		TypeChaos: "Mega Wave", // Тип катастрофы
-		LogChaos: []ActionApocalypse{ // Лог действий с сохранением времени
-			{Action: "MegaSuname", t: time.Now()},
-			{Action: "MegaSuname2", t: time.Now().Add(1 * time.Minute)}, // Добавляем 1 минуту
-			{Action: "MegaSuname3", t: time.Now().Add(2 * time.Minute)}, // Добавляем 2 минуты
-			{Action: "MegaSuname4", t: time.Now().Add(3 * time.Minute)}, // Добавляем 3 минуты
-			{Action: "MegaSuname5", t: time.Now().Add(4 * time.Minute)}, // Добавляем 4 минуты
-		},
+	rand.New(rand.NewSource(time.Now().Unix()))
+
+	u := genetateUser(100)
+
+	for i, user := range u {
+		write(user, i+1)
+	}
+}
+
+func write(u Apocalypse, id int) error {
+	fmt.Printf("writen user: id - %d", u.Name)
+
+	filename := fmt.Sprintf("logs/uid_%d.txt", id)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
 	}
 
-	// Вызываем метод для вывода данных
-	Tsunami.createTableCahce()
+	_, err = file.WriteString(u.createTableCahce())
+	return nil
+}
+
+func genetateUser(count int) []Apocalypse {
+	user := make([]Apocalypse, count)
+
+	for i := 1; i < count; i++ {
+		user[i] = Apocalypse{
+			Name:      fmt.Sprintf("apocalypse%d reg", i),
+			TypeChaos: "storm",
+			LogChaos:  generateLog(rand.Intn(1000)),
+		}
+	}
+	return user
+}
+
+func generateLog(count int) []ActionApocalypse {
+	log := make([]ActionApocalypse, count)
+
+	for i := 1; i < count; i++ {
+		log[i] = ActionApocalypse{t: time.Now(), Action: ActArr[rand.Intn(len(ActArr)-1)]}
+	}
+	return log
 }
