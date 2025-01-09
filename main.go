8c274a5969db31c6b0a5d45334ac78b4442484ab
icontaylore <icontaylore@icloud.com>
2025-01-09 16:28:08 +0400

    односвязный список

--- main.go
+++ main.go
@@ -1,32 +1,37 @@
 package main
 
-// односвязанный список
+// Односвязанный список
 type LinkedList struct {
 	Head *Node
 	Tail *Node
 }
 
+// Структура узла
 type Node struct {
 	Data int
 	Next *Node
 }
 
+// Добавление элемента в начало списка
 func (l *LinkedList) PushFront(i int) {
 	n := &Node{Data: i, Next: l.Head}
 
+	// Если список пуст, новый элемент будет и Head, и Tail
 	if l.Head == nil {
 		l.Tail = n
-		l.Head = n
-	} else if l.Head != nil {
-		l.Head = n
 	}
+	l.Head = n
 }
+
+// Добавление элемента в конец списка
 func (l *LinkedList) PushBack(i int) {
 	n := &Node{Data: i, Next: nil}
 
+	// Если список пуст, новый элемент будет и Head, и Tail
 	if l.Head == nil {
-		l.Tail, l.Head = n, n
-	} else if l.Head != nil {
+		l.Head, l.Tail = n, n
+	} else {
+		// Если список не пуст, добавляем элемент в конец
 		l.Tail.Next = n
 		l.Tail = n
 	}
@@ -34,6 +39,6 @@ func (l *LinkedList) PushBack(i int) {
 
 func main() {
 	l1 := &LinkedList{}
-	l1.PushBack(1)
-	l1.PushFront(2)
+	l1.PushBack(1)  // Добавляем 1 в конец
+	l1.PushFront(2) // Добавляем 2 в начало
 }
