8c7ae966562800078b9f96040761c0fe6e9c01a2
icontaylore <icontaylore@icloud.com>
2025-01-14 19:58:42 +0400

    двусвязный список, мейби верный, пока не чекал

--- main.go
+++ main.go
@@ -3,145 +3,55 @@ package main
 import (
 	"container/list"
 	"fmt"
+	"strings"
 )
 
-// MaxLenList - структура, оборачивающая двусвязный список с ограничением на максимальную длину
-type MaxLenList struct {
-	List   *list.List // Основной список
-	MaxLen int        // Максимальная длина списка
+type Deque struct {
+	Stack *list.List
 }
 
-type Node struct {
+func (s *Deque) createStackForFormula(str string) {
+	newStr := strings.ReplaceAll(str, " ", "")
+	arrStr := []rune{}
+
+	for _, v := range newStr {
+		arrStr = append(arrStr, v)
+	}
+
+	s.goToQueStack(arrStr)
 }
 
-// MaxLenCreate - функция-конструктор для создания списка с ограничением длины
-func MaxLenCreate(max int) *MaxLenList {
-	return &MaxLenList{
-		List:   list.New(), // Создаём новый двусвязный список
-		MaxLen: max,        // Задаём максимальную длину
-	}
-}
+func (s *Deque) goToQueStack(str []rune) {
 
-// PushBack - метод для добавления элемента в конец списка
-func (l *MaxLenList) PushBack(i int) {
-	// Если текущая длина списка больше или равна максимальной, удаляем первый элемент
-	if l.List.Len() >= l.MaxLen {
-		l.List.Remove(l.List.Front()) // Удаляем самый старый элемент (FIFO)
-	}
+	for i, v := range str {
+		if string(v) == "(" {
+			s.Stack.PushFront(int(i))
 
-	// Добавляем новый элемент в конец списка
-	l.List.PushBack(i)
-}
+		} else if string(v) == ")" {
 
-func (l *MaxLenList) PushFront(i int) {
-	if l.List.Len() >= l.MaxLen {
-		l.List.Remove(l.List.Back())
-	}
+			slice := str[s.Stack.Front().Value.(int) : i+1]
+			s.outSlice(slice)
+			s.Stack.Remove(s.Stack.Front())
 
-	l.List.PushFront(i)
-}
-
-func (l *MaxLenList) Insert(i int, j int) {
-	n := &list.Element{Value: j}
-
-	if l.List == nil {
-		return
-	}
-
-	for cur := l.List.Front(); cur != nil; cur = cur.Next() {
-		if cur.Value == i {
-			l.List.InsertAfter(n, cur)
-			return
-		}
-	}
-}
-
-// Items - метод для получения всех элементов списка в виде среза
-func (m *MaxLenList) Items() []interface{} {
-	var items []interface{} // Срез для хранения элементов
-
-	// Проходим по списку, начиная с первого элемента, и добавляем значения в срез
-	for e := m.List.Front(); e != nil; e = e.Next() {
-		items = append(items, e.Value) // Добавляем значение текущего элемента
-	}
-	return items
-}
-
-func (l *MaxLenList) PopLeft(i int) interface{} {
-	if l.List == nil {
-		return nil
-	}
-
-	right := l.List.Front()
-	l.List.Remove(right)
-	return right.Value
-}
-
-func (l *MaxLenList) PopRight(i int) interface{} {
-	if l.List == nil {
-		return nil
-	}
-
-	left := l.List.Back()
-	l.List.Remove(left)
-	return left.Value
-}
-
-func (l *MaxLenList) Pop(i int) interface{} {
-	if l.List == nil {
-		return nil
-	}
-
-	for e := l.List.Front(); e != nil; e = e.Next() {
-		if e.Value == i {
-			return l.List.Remove(e)
 		}
 	}
 
-	return nil
 }
 
-func (l *MaxLenList) Extend(i []int) interface{} {
-	if l.List == nil {
-		return nil
+func (s *Deque) outSlice(arr []rune) string {
+	str := ""
+
+	for _, v := range arr {
+		str += string(v)
 	}
 
-	for _, v := range i {
-		l.PushBack(v)
-	}
-
-	return l.Items()
-}
-
-func (l *MaxLenList) ExtendLeft(i []int) interface{} {
-	if l.List == nil {
-		return nil
-	}
-
-	for _, v := range i {
-		l.PushFront(v)
-	}
-
-	return l.Items()
+	fmt.Println(str)
+	return str
 }
 
 func main() {
-	// Исходный массив данных
-	arr := []int{1, 2, 3, 4, 5}
+	s1 := &Deque{Stack: list.New()}
+	data := "2 + 3 * (1 - 5 - (33 * x - 5)) + (a - b)"
 
-	// Создаём список с максимальной длиной 5
-	dq := MaxLenCreate(5)
-
-	// Заполняем список элементами массива
-	for _, v := range arr {
-		dq.PushBack(v)
-	}
-
-	// Добавляем ещё один элемент, что вызывает удаление самого старого элемента
-	fmt.Println(dq.ExtendLeft([]int{-3, -2, -1}))
-	fmt.Println(dq.Extend([]int{6, 7, 8}))
-
-	//Выводим элементы списка
-	fmt.Println(dq.Items())
-
+	s1.createStackForFormula(data)
 }
