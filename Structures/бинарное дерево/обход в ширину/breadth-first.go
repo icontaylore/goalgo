f845ad2e1b4d7f1bddf56c3e4e948cae56058102
icontaylore <icontaylore@icloud.com>
2025-01-18 18:26:00 +0400

    bfs

---
+++ Structures/бинарное дерево/обход в ширину/breadth-first.go
@@ -0,0 +1,56 @@
+package обход_в_ширину
+
+import "fmt"
+
+type Node struct {
+	Data  int
+	Left  *Node
+	Right *Node
+}
+
+type Tree struct {
+	Root *Node
+}
+
+func (t *Tree) ShowWide() *Node {
+	if t.Root == nil {
+		return nil
+	}
+
+	queue := []*Node{t.Root}
+
+	for len(queue) > 0 {
+		lvl := len(queue)
+
+		for i := 0; i < lvl; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			fmt.Print(node.Data, " ")
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		fmt.Println()
+	}
+
+	return nil
+}
+
+func main() {
+	t1 := &Tree{}
+
+	t1.Push(8)
+	t1.Push(5)
+	t1.Push(6)
+	t1.Push(4)
+	t1.Push(10)
+	t1.Push(9)
+	t1.Push(11)
+
+	t1.ShowWide()
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,149 @@
-package awesomeProject5
+package main
+
+import "fmt"
+
+type Node struct {
+	Data  int
+	Left  *Node
+	Right *Node
+}
+
+type Tree struct {
+	Root *Node
+}
+
+func (t *Tree) Push(i int) {
+	t.Root = PushItem(t.Root, i)
+}
+
+// PUSH AND ITEM
+func PushItem(node *Node, i int) *Node {
+	n := &Node{Data: i}
+
+	if node == nil {
+		return n
+	}
+
+	if i < node.Data {
+		node.Left = PushItem(node.Left, i)
+	} else if i > node.Data {
+		node.Right = PushItem(node.Right, i)
+	}
+	return node
+}
+
+func (t *Tree) Item() {
+	stack := []*Node{}
+	cur := t.Root
+
+	for cur != nil || len(stack) > 0 {
+		for cur != nil {
+			stack = append(stack, cur)
+			cur = cur.Left
+		}
+
+		cur = stack[len(stack)-1]
+		fmt.Println(cur.Data)
+		stack = stack[:len(stack)-1]
+
+		cur = cur.Right
+	}
+}
+
+// PUSH AND ITEM
+
+// POP AND ELEM
+func (t *Tree) Pop(i int) {
+	t.Root = PopOut(t.Root, i)
+}
+
+func PopOut(node *Node, i int) *Node {
+	if node.Data == i {
+		small := FindSmall(node.Right)
+		node.Data = small.Data
+		node.Right = DeleteElem(node.Right, small.Data)
+	}
+
+	if i < node.Data {
+		node.Left = DeleteElem(node.Left, i)
+	} else if i > node.Data {
+		node.Right = DeleteElem(node.Right, i)
+	}
+
+	return node
+}
+
+func FindSmall(node *Node) *Node {
+	for node.Left == nil {
+		return node
+	}
+
+	return FindSmall(node.Left)
+}
+
+func DeleteElem(node *Node, i int) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if i > node.Data {
+		DeleteElem(node.Right, i)
+	} else if i < node.Data {
+		DeleteElem(node.Left, i)
+	} else {
+		if node.Left == nil {
+			return node.Right
+		} else if node.Right == nil {
+			return node.Left
+		}
+
+		smal := FindSmall(node.Right)
+		node.Data = smal.Data
+		node.Right = DeleteElem(node.Right, smal.Data)
+	}
+
+	return node
+}
+
+func (t *Tree) ShowWide() *Node {
+	if t.Root == nil {
+		return nil
+	}
+
+	queue := []*Node{t.Root}
+
+	for len(queue) > 0 {
+		lvl := len(queue)
+
+		for i := 0; i < lvl; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			fmt.Print(node.Data, " ")
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		fmt.Println()
+	}
+
+	return nil
+}
+
+func main() {
+	t1 := &Tree{}
+
+	t1.Push(8)
+	t1.Push(5)
+	t1.Push(6)
+	t1.Push(4)
+	t1.Push(10)
+	t1.Push(9)
+	t1.Push(11)
+
+	t1.ShowWide()
+
+}
